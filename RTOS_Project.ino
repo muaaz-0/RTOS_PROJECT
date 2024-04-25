b18a34258cdf7836d46d8f24072159d44f94fc44
muaaz-0 <74170095+muaaz-0@users.noreply.github.com>
2024-04-26 01:48:05 +0500

    Update RTOS_Project.ino

--- RTOS_Project.ino
+++ RTOS_Project.ino
@@ -4,7 +4,8 @@ task1: i am noticing the time to task1 and 3, counting the no of tasks
 task2: in task2 i am noticing the time to task2, counting the no of tasks3
 task3: Using all serial commands, 
 task4: blinking leds on every batch completion
-task5: showing task time on Oled
+task5: showing task time on Oled1
+task6: showing task time on Oled2
 */
 
 #include <RTClib.h>
